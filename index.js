aa44562ede26a4e0bd3d44984974818d5596e766
neslnwfaro <neslnwfaro003@hotmail.com>
2022-01-11 13:04:39 +0700

    main2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const router = require('./routes/myRouter')
-
+// const router = require('./routes/myRouter')
+const path = require('path')
 const app = express()
+app.use(express.static(path.join(__dirname,'public')))
 
-app.use(router)
+// app.use(router)
 
 app.listen(8080,()=>{
     console.log("Start Server Port 8080");
